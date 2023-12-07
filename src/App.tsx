23bce3a35b3ce872922200fb3ba36b72bd903ed2
Jaz Singh <jazeee@gmail.com>
2023-12-06 16:27:43 -0800

    Add hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { CssBaseline, ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider, Typography } from "@mui/material";
 import { APP_THEME } from "./Mui/theme";
 
 export function App() {
   return (
     <ThemeProvider theme={APP_THEME}>
       <CssBaseline />
+      <Typography>Hello World</Typography>
     </ThemeProvider>
   );
 }
