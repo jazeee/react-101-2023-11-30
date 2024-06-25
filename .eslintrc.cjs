cfb038c9c55806bb2162942fe0b310162572b6ef
Jaz Singh <jazeee@gmail.com>
2024-06-25 09:59:29 -0700

    Update lint and add missing package

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,21 +9,21 @@ module.exports = {
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', "unused-imports", 'prettier'],
+  plugins: ['react-refresh', 'unused-imports', 'prettier'],
   rules: {
-    "prettier/prettier": "error",
-    "unused-imports/no-unused-imports": "error",
-    "no-implicit-coercion": "error",
+    'prettier/prettier': 'error',
+    'unused-imports/no-unused-imports': 'error',
+    'no-implicit-coercion': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
-        "singleQuote": true,
-        "trailingComma": "all",
-      }
-    ]
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
   },
-}
+};
